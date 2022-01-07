fa1f2e4158568eed617cfc8e5e07f03f1fd61e03
Thea Flowers <me@thea.codes>
2022-01-07 14:46:16 -0500

    Update to gather orders from squarespace and beedle

--- generate.py
+++ generate.py
@@ -5,8 +5,10 @@ import os
 
 API_KEY = os.environ["SS_API_KEY"]
 API_SECRET = os.environ["SS_API_SECRET"]
-STORE_ID = os.environ.get("SS_STORE_ID", None)
+STORE_IDS = os.environ.get("SS_STORE_ID", None)
 
+if STORE_IDS:
+    STORE_IDS = STORE_IDS.split(" ")
 
 def make_authorization_header(key, secret):
     encoded = base64.urlsafe_b64encode(f"{key}:{secret}".encode("utf-8"))
@@ -51,7 +53,7 @@ class Shipstation:
 
 
 def list_stores(ss):
-    print("No store specified, please set SS_STORE_ID to one of the following:")
+    print("No store specified, please set SS_STORE_ID to one or more of the following:")
     stores = ss.list_stores()
 
     for store in stores:
@@ -60,7 +62,7 @@ def list_stores(ss):
 
 def count_shipped_orders_over_last_month(ss):
     start_date = (datetime.date.today() - datetime.timedelta(days=31)).strftime("%Y-%m-%d")
-    shipments = ss.list_shipments(store_id=STORE_ID, page_size=500, ship_date_start=start_date)
+    shipments = ss.list_shipments(store_id=STORE_IDS, page_size=500, ship_date_start=start_date)
 
     last_ship_date = shipments["shipments"][-1]["shipDate"]
     last_ship_date = datetime.datetime.strptime(last_ship_date, "%Y-%m-%d")
@@ -70,14 +72,19 @@ def count_shipped_orders_over_last_month(ss):
 
 def generate_order_list(ss):
     shipped_in_last_month, last_shipped = count_shipped_orders_over_last_month(ss)
-    orders = ss.list_orders(order_status="awaiting_shipment", store_id=STORE_ID, page_size=500)
+
+    orders = []
+
+    for store_id in STORE_IDS:
+        results = ss.list_orders(order_status="awaiting_shipment", store_id=store_id, page_size=500)
+        orders.extend(results["orders"])
 
     with open("docs/queue.md", "w") as fh:
         print(f"We have shipped **{shipped_in_last_month}** orders over the last month, and the last order we shipped went out on {last_shipped}.\n\n", file=fh)
         print("| position in queue | order number | order date |", file=fh)
         print("| - | - | - |", file=fh)
 
-        for n, order in enumerate(orders["orders"], 1):
+        for n, order in enumerate(orders, 1):
             order_date = datetime.datetime.fromisoformat(order["orderDate"].rsplit(".", 1)[0]).date()
             combined = "<sup title='This order was combined with another'>*</sup>" if order["advancedOptions"]["mergedOrSplit"] else ""
 
@@ -85,13 +92,17 @@ def generate_order_list(ss):
 
 
 def generate_preorder_list(ss):
-    orders = ss.list_orders(order_status="on_hold", store_id=STORE_ID, page_size=500)
+    orders = []
+
+    for store_id in STORE_IDS:
+        results = ss.list_orders(order_status="on_hold", store_id=store_id, page_size=500)
+        orders.extend(results["orders"])
 
     with open("docs/preorder-queue.md", "w") as fh:
         print("| position in queue | order number | order date |", file=fh)
         print("| - | - | - |", file=fh)
 
-        for n, order in enumerate(orders["orders"], 1):
+        for n, order in enumerate(orders, 1):
             order_date = datetime.datetime.fromisoformat(order["orderDate"].rsplit(".", 1)[0]).date()
             hold_date = datetime.datetime.fromisoformat(order["holdUntilDate"].rsplit(".", 1)[0]).date()
             combined = "<sup title='This order was combined with another'>*</sup>" if order["advancedOptions"]["mergedOrSplit"] else ""
@@ -101,7 +112,7 @@ def generate_preorder_list(ss):
 def main():
     ss = Shipstation(API_KEY, API_SECRET)
 
-    if STORE_ID is None:
+    if STORE_IDS is None:
         list_stores(ss)
     else:
         generate_order_list(ss)
